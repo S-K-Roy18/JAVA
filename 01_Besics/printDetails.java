26d603b3efc3d44222402ec0488c1a58071be8dd
S-K-Roy18 <suryarahulroy321@gmail.com>
2026-07-11 22:54:10 +0530

    First Java programs

---
+++ 01_Besics/printDetails.java
@@ -0,0 +1,9 @@
+//Print your name, age, and city.
+
+public class printDetails{
+    public static void main(String[] a){
+        System.out.println("my name is: SURYA");
+        System.out.println("age:21");
+        System.out.println("Address: Kolkata, West Bengal");
+    }
+}
