e15193c7499c4ef540f2281748f1f469518f5135
S-K-Roy18 <suryarahulroy321@gmail.com>
2026-07-14 01:05:44 +0530

    right-angled--

--- If-Else/rightangle.java
+++ If-Else/rightangle.java
@@ -10,6 +10,23 @@
 
 
 
+// import java.util.*;
+// public class rightangle{
+//     public static void main(String[] args){
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("Enter three edges of triangle:");
+//         int a= sc.nextInt();
+//         int b= sc.nextInt();
+//         int c= sc.nextInt();
+//         if(a*a+b*b==c*c){
+//             System.out.println("Its a right-angled triangle.");
+//         }
+//         else{
+//             System.out.println("Its not right-angled.");
+//         }
+//     }
+// }
+
 import java.util.*;
 public class rightangle{
     public static void main(String[] args){
@@ -18,11 +35,34 @@ public class rightangle{
         int a= sc.nextInt();
         int b= sc.nextInt();
         int c= sc.nextInt();
-        if(a*a+b*b==c*c){
-            System.out.println("Its a right-angled triangle.");
+        if(a+b>c||b+c>a||c+a>b){
+            if(a>b&&a>c){
+                if(b*b+c*c==a*a){
+                    System.out.println("Its a right-angled triangle.");
+                }
+                else{
+                    System.out.println("Its not right-angled.");
+                }
+            }
+            if(b>c&&b>a){
+                if(a*a+c*c==b*b){
+                    System.out.println("Its a right-angled triangle.");
+                }
+                else{
+                    System.out.println("Its not right-angled.");
+                }
+            }
+            if(c>b&&c>b){
+                if(b*b+a*a==c*c){
+                    System.out.println("Its a right-angled triangle.");
+                }
+                else{
+                    System.out.println("Its not right-angled.");
+                }
+            }
         }
         else{
-            System.out.println("Its not right-angled.");
+            System.out.println("Invalid edges.");
         }
     }
 }
